0cdff1d350b2eeb2a5772fed903471a91b46464a
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-05-12 06:05:04 -0600

    used encapsulation , Inheritance and accessors

--- Inheritance.cs
+++ Inheritance.cs
@@ -1,25 +1,25 @@
-﻿using System;
+﻿// using System;
 
-namespace inheritance
-{
-    class Group {
-        public string groupName= " My 2020 Painting collection";
-        public void greet(){
-            Console.WriteLine("Welcome to my ART GALLERY! ");
+// namespace C_OOP_Assignment
+// {
+//     class Group {
+//         public string groupName= " My 2020 Painting collection";
+//         public void greet(){
+//             Console.WriteLine("Welcome to my ART GALLERY! ");
 
-    }
-    class Painting:Group {
-        public string paintingName="Life in West Edmonton Mall ";
-    }
+//     }
+//     class Painting:Group {
+//         public string paintingName="Life in West Edmonton Mall ";
+//     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Painting newPainting = new Painting();
-            newPainting.greet();
-            Console.WriteLine(newPainting.paintingName + " is the first addition in "+ newPainting.groupName);
-        }
-    }
-}
-}
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Painting newPainting = new Painting();
+//             newPainting.greet();
+//             Console.WriteLine(newPainting.paintingName + " is the first addition in "+ newPainting.groupName);
+//         }
+//     }
+// }
+// }
